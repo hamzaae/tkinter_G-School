115c694d446d1e6ccfb7fdab2483904ba27cb90d
hamzaae <motassimhamza99@gmail.com>
2023-03-18 15:22:01 +0100

    added inputs

--- login.py
+++ login.py
@@ -13,24 +13,30 @@ bg = tk.PhotoImage(file="slide.png")
 img = tk.Label(root, image=bg)
 img.place(x=-360, y=0)
 
-frame = tk.Frame(root, width=340, height=405,)
+frame = tk.Frame(root, width=340, height=405,bg='white')
 frame.place(x=360)
 
-head = tk.Label(frame,text='ENSAH G-SCHOOL LOGIN',font="Times 15 italic bold")
-head.place(x=80,y=35)
+head = tk.Label(frame,text='ENSAH G-SCHOOL LOGIN',font="Times 15 italic bold",bg='white')
+head.place(x=75,y=45)
 
 # Show logo
 logo = tk.PhotoImage(file="logo-ensah_50x50.png")
-img_logo = tk.Label(frame, image=logo)
-img_logo.place(x=10, y=20)
+img_logo = tk.Label(frame, image=logo,bg='white')
+img_logo.place(x=10, y=30)
+
+#
+user = tk.Entry(frame, width= 25, fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',18,'bold'))
+user.place(x=30, y=120)
+user.insert(0,'Username')
+
+pswrd = tk.Entry(frame, width= 25, fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',18,'bold'))
+pswrd.place(x=30, y=190)
+pswrd.insert(0,'Password')
+
+tk.Frame(frame,width=295, height=2,bg='black').place(x=25,y=157)
+tk.Frame(frame,width=295, height=2,bg='black').place(x=25,y=227)
 
 
-
-
-'''l1 = tk.Label(root, text="Login name:", fg="black", bg="white")
-l2 = tk.Label(root, text="Password:", fg="black", bg="white")
-l1.grid(row=0, column=1)
-l2.grid(row=1, column=1)'''
 
 
 
