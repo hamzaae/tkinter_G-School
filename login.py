55196d8e501a886ff9da59f29549155f5d1158a4
hamzaae <motassimhamza99@gmail.com>
2023-03-24 14:06:30 +0000

    added settings tab and dark/light mode

--- login.py
+++ login.py
@@ -12,52 +12,63 @@ bg = tk.PhotoImage(file="media\\slide.png")
 img = tk.Label(root, image=bg)
 img.place(x=-360, y=0)
 
-frame = tk.Frame(root, width=340, height=405,bg='white')
+frame = tk.Frame(root, width=340, height=405, bg='white')
 frame.place(x=360)
 
 # Show logo and heading
-head = tk.Label(frame,text='ENSAH G-SCHOOL LOGIN',font=("Helvetica", 16, 'bold'),bg='white')
-head.place(x=68,y=45)
+head = tk.Label(frame, text='ENSAH G-SCHOOL LOGIN', font=("Helvetica", 16, 'bold'), bg='white')
+head.place(x=68, y=45)
 
 logo = tk.PhotoImage(file="media\\logo-ensah_50x50.png")
-img_logo = tk.Label(frame, image=logo,bg='white')
+img_logo = tk.Label(frame, image=logo, bg='white')
 img_logo.place(x=8, y=30)
+
 
 ### Username Input behavior
 def on_enter(e):
     user.delete(0, 'end')
+
+
 def on_leave(e):
-    name=user.get()
+    name = user.get()
     if name == "":
         user.insert(0, 'Username')
 
+
 # Add inputs and ligne decorator
-user = tk.Entry(frame, width= 25, fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',18,'bold'))
+user = tk.Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 18, 'bold'))
 user.place(x=30, y=130)
-user.insert(0,'Username')
+user.insert(0, 'Username')
 user.bind('<FocusIn>', on_enter)
 user.bind('<FocusOut>', on_leave)
+
+
 ### Password Input behavior
 def on_enter(e):
     pswrd.delete(0, 'end')
+
+
 def on_leave(e):
     name = pswrd.get()
     if name == "":
         pswrd.insert(0, 'Password')
-pswrd = tk.Entry(frame, width= 25, fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',18,'bold'))
+
+
+pswrd = tk.Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 18, 'bold'))
 pswrd.place(x=30, y=200)
-pswrd.insert(0,'Password')
+pswrd.insert(0, 'Password')
 pswrd.bind('<FocusIn>', on_enter)
 pswrd.bind('<FocusOut>', on_leave)
 
-tk.Frame(frame,width=295, height=2,bg='black').place(x=25,y=167)
-tk.Frame(frame,width=295, height=2,bg='black').place(x=25,y=237)
+tk.Frame(frame, width=295, height=2, bg='black').place(x=25, y=167)
+tk.Frame(frame, width=295, height=2, bg='black').place(x=25, y=237)
+
 
 ### sign_in behavior
 def on_signin():
     username = user.get()
     password = pswrd.get()
-    if access :
+    if access:
         pass
     elif ():
         pass
@@ -66,14 +77,13 @@ def on_signin():
 
 
 # Sign in and sign up buttons
-signin_btn = tk.Button(frame,width=39,pady=7,text='Sign in',bg='#6cc570',fg='white',border=0, command=on_signin)
+signin_btn = tk.Button(frame, width=39, pady=7, text='Sign in', bg='#6cc570', fg='white', border=0, command=on_signin)
 signin_btn.place(x=35, y=270)
-signup_lbl = tk.Label(frame,text="New user? Create your account ",fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
+signup_lbl = tk.Label(frame, text="New user? Create your account ", fg='black', bg='white',
+                      font=('Microsoft YaHei UI Light', 9))
 signup_lbl.place(x=70, y=310)
-signup_btn = tk.Button(frame,cursor='hand2',width=4,border=0,text="here",fg='#3d85c6',bg='white',font=('Microsoft YaHei UI Light',9,'bold'))
+signup_btn = tk.Button(frame, cursor='hand2', width=4, border=0, text="here", fg='#3d85c6', bg='white',
+                       font=('Microsoft YaHei UI Light', 9, 'bold'))
 signup_btn.place(x=250, y=310)
-
-
-
 
 root.mainloop()
--- main_home.py
+++ main_home.py
@@ -1,3 +1,4 @@
+import tkinter
 import tkinter as tk
 from tkinter import ttk
 import webbrowser
@@ -9,16 +10,20 @@ window.geometry('900x550')
 window.resizable(False, False)
 
 # Initialize notebook
-style = ttk.Style(window)
-style.configure('lefttab.TNotebook', tabposition='ws')
-notebook = ttk.Notebook(window,style='lefttab.TNotebook',width=900,height=550)
+style0 = ttk.Style(window)
+style0.configure('lefttab.TNotebook', tabposition='ws')
+style1 = ttk.Style()
+style1.configure('Custom.TFrame', background="white")
+notebook = ttk.Notebook(window, style='lefttab.TNotebook', width=900, height=550)
+
 
 # tabs config: creation, icons, add to notebook
-home_tab = ttk.Frame(notebook)
-students_tab = ttk.Frame(notebook)
-hr_tab = ttk.Frame(notebook)
-settings_tab = ttk.Frame(notebook)
-about_tab = ttk.Frame(notebook)
+home_tab = ttk.Frame(notebook, style='Custom.TFrame')
+students_tab = ttk.Frame(notebook, style='Custom.TFrame')
+hr_tab = ttk.Frame(notebook, style='Custom.TFrame')
+settings_tab = ttk.Frame(notebook, style='Custom.TFrame')
+about_tab = ttk.Frame(notebook, style='Custom.TFrame')
+
 
 t1 = tk.PhotoImage(file='media\\home.png')
 t2 = tk.PhotoImage(file='media\\stdt.png')
@@ -26,11 +31,11 @@ t3 = tk.PhotoImage(file='media\\tchr.png')
 t4 = tk.PhotoImage(file='media\\sett.png')
 t5 = tk.PhotoImage(file='media\\hlp.png')
 
-notebook.add(home_tab, text='\n\n\n   HOME    \n\n\n',image=t1, compound='left')
-notebook.add(students_tab, text='\n\n\nSTUDENTS\n\n\n',image=t2, compound='left')
-notebook.add(hr_tab, text='\n\n\n   STAFFS   \n\n\n',image=t3, compound='left')
-notebook.add(settings_tab, text='\n\n\n SETTINGS \n\n\n',image=t4, compound='left')
-notebook.add(about_tab, text='\n\n\n   ABOUT   \n\n\n',image=t5, compound='left')
+notebook.add(home_tab, text='\n\n\n   HOME    \n\n\n', image=t1, compound='left')
+notebook.add(students_tab, text='\n\n\nSTUDENTS\n\n\n', image=t2, compound='left')
+notebook.add(hr_tab, text='\n\n\n   STAFFS   \n\n\n', image=t3, compound='left')
+notebook.add(settings_tab, text='\n\n\n SETTINGS \n\n\n', image=t4, compound='left')
+notebook.add(about_tab, text='\n\n\n   ABOUT   \n\n\n', image=t5, compound='left')
 
 notebook.pack()
 
@@ -44,19 +49,59 @@ notebook.pack()
 with open('media\\about_text', 'r') as abt:
     # open about text and read content then pack it to about tab
     about_text = abt.read()
-    abt_lbl = tk.Label(about_tab, text=about_text,fg='black',bg='white',font=('Microsoft YaHei UI Light',11,'bold'),width=900,height=550)
+    abt_lbl = tk.Label(about_tab, text=about_text, fg='black',
+                       font=('Microsoft YaHei UI Light', 11, 'bold'), width=900, height=550,bg="white")
     abt_lbl.pack()
+
 
     # Define a callback function
     def callback(url):
         webbrowser.open_new_tab(url)
 
+
     # Create a Label to display the link
-    link = tk.Label(about_tab, text="https://ensah.ma/", font=('Helveticabold', 15), fg="blue",bg='white', cursor="hand2")
-    link.place(x=320,y=520)
+    link = tk.Label(about_tab, text="https://ensah.ma/", font=('Helveticabold', 15), fg="blue",bg="white",
+                    cursor="hand2")
+    link.place(x=320, y=520)
     link.bind("<Button-1>", lambda e: callback("https://ensah.ma/"))
 
+## Settings tab
+frame_user = tkinter.LabelFrame(settings_tab, text="User settings", font=('Helveticabold', 15),bg="white")
+frame_user.pack(fill="both", expand=1)
+frame_general = tkinter.LabelFrame(settings_tab, text="General settings",font=('Helveticabold', 15),bg="white")
+frame_general.pack(fill="both", expand=1)
 
+
+v = tkinter.StringVar(frame_general, "1")
+def set_light_mode():
+    style1.configure('Custom.TFrame', background='white')
+    frame_general.configure(bg='white',fg='black')
+    light_rb.configure(bg="white",fg="black")
+    dark_rb.configure(bg="white",fg="black")
+    language_lbl.configure(bg='white', fg='black')
+    frame_user.configure(bg='white',fg='black')
+    abt_lbl.configure(bg='white',fg='black')
+    link.configure(bg='white',fg='blue')
+
+def set_dark_mode():
+    style1.configure('Custom.TFrame', background='#26242f')
+    frame_general.configure(bg='#26242f',fg="white")
+    light_rb.configure(bg='#26242f',fg='white')
+    dark_rb.configure(bg='#26242f',fg='white')
+    language_lbl.configure(bg='#26242f',fg='white')
+    frame_user.configure(bg='#26242f', fg="white")
+    abt_lbl.configure(bg='#26242f', fg="white")
+    link.configure(bg='#26242f', fg="blue")
+
+light_rb = tkinter.Radiobutton(frame_general, text=" Light mode ", variable=v,
+                    value="1",font=('Microsoft YaHei UI Light', 11, 'bold'),bg='white', command=set_light_mode)
+light_rb.place(x=0,y=30)
+dark_rb = tkinter.Radiobutton(frame_general, text=" Dark mode ", variable=v,
+                    value="2",font=('Microsoft YaHei UI Light', 11, 'bold'),bg='white', command=set_dark_mode)
+dark_rb.place(x=0,y=70)
+language_lbl = tkinter.Label(frame_general, text="      Language ",
+              font=('Microsoft YaHei UI Light', 11, 'bold'), bg="white")
+language_lbl.place(x=0, y=110)
 
 
 # mainloop
